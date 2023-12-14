ccdcd50ad5dd439de819d667f74a762060630c9d
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2023-12-14 15:31:16 +0100

    Create task24.py

---
+++ tasks_14Dec/task24.py
@@ -0,0 +1,14 @@
+# Task 24: Import the “sqrt” function from the “math” module and use it to find the square root of a number.
+from math import sqrt
+
+
+def run():
+    num1 = 16
+    num2 = 19
+
+    print(sqrt(num1))
+    print(sqrt(num2))
+
+
+if __name__ == "__main__":
+    run()
