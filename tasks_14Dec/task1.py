9de23fb4dfec1af4f41150cb966b5bf2299779d6
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2023-12-14 12:59:14 +0100

    Create task1.py

---
+++ tasks_14Dec/task1.py
@@ -0,0 +1,25 @@
+
+def run():
+    for i in range(10):
+        i += 1
+        if i % 2 != 0:
+            continue
+        print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    pass
+
+
+if __name__ == "__main__":
+    run()
