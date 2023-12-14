6a1971f9c5f7e44f0fb6a5a1522ebc6715740a56
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2023-12-14 15:27:41 +0100

    Create task14.py

---
+++ tasks_14Dec/task14/task14.py
@@ -0,0 +1,13 @@
+# Task 14: Write a program that opens a file, writes "Hello, File!" to it, and closes the file.
+
+
+def run():
+    file_path = "file.txt"
+    with open(file_path, 'w') as file:
+        text = "Hello, File!"
+        file.write(text)
+        print(f"Skrev '{text}' til filen '{file_path}'")
+
+
+if __name__ == "__main__":
+    run()
