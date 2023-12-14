24fa908898a11ad3ef0f896c04355c04c49340ee
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2023-12-14 15:31:23 +0100

    Create task28.py

---
+++ tasks_14Dec/task28.py
@@ -0,0 +1,10 @@
+# Task 28: Write a program that prints the current date and time using the datetime module.
+from datetime import datetime
+
+
+def run():
+    print(datetime.now())
+
+
+if __name__ == "__main__":
+    run()
