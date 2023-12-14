88d01a38124ae3732d5bd7976490f76c1f595ade
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2023-12-14 15:27:34 +0100

    Create task13.py

---
+++ tasks_14Dec/task13/task13.py
@@ -0,0 +1,18 @@
+# Task 13: Write a program that opens a file, reads its content, and closes the file.
+
+
+def run():
+    file_path = "file.txt"
+
+    try:
+        with open(file_path, 'r') as file:
+
+            file_content = file.read()
+            print(file_content)
+
+    except FileNotFoundError:
+        print("Fant ikke filen!")
+
+
+if __name__ == "__main__":
+    run()
