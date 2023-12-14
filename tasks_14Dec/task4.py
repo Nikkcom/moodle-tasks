e817266ead8ee9395c99cd0d23a52dd23fab922b
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2023-12-14 13:16:40 +0100

    Update task4.py

--- tasks_14Dec/task4.py
+++ tasks_14Dec/task4.py
@@ -1,11 +1,12 @@
 def run():
-    for i in range(10):
-        fibgen = fib_gen()
+    fibgen = fib_gen()
+    for i in range(20):
         print(next(fibgen))
 
 
 def fib_gen():
     a, b = 0, 1
+
     while True:
         yield a
         a, b = b, a + b
