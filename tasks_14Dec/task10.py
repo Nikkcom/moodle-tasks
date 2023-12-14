8df438eb1e811ac9466ae24b412816e969ba5d35
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2023-12-14 15:27:11 +0100

    Create task10.py

---
+++ tasks_14Dec/task10.py
@@ -0,0 +1,12 @@
+# Task 10: Import the randint function from the random module and use it to generate a random number.
+from random import randint
+
+
+def run():
+    num1 = randint(0, 10)
+
+    print(num1)
+
+
+if __name__ == "__main__":
+    run()
