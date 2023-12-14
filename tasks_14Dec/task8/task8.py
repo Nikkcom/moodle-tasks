0a648ff4ea922d5087a10f60e292cdb562bb1b96
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2023-12-14 15:31:39 +0100

    Create task8.py

---
+++ tasks_14Dec/task8/task8.py
@@ -0,0 +1,14 @@
+# Task 8: Create a separate Python file with a function that prints "Hello, World!" and import and use that function
+# in another file.
+import hello
+
+
+def run():
+
+    hello.hello()
+
+
+if __name__ == "__main__":
+    run()
+
+
